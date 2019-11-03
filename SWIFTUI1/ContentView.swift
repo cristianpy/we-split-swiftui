881f45e47568c7b144dc65dacba40498a85e4c5f
Cristian Paniagua <cristianramonpaniagua@gmail.com>
2019-11-03 14:34:39 -0300

    Initial Commit

---
+++ SWIFTUI1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SWIFTUI1
+//
+//  Created by Cristian Paniagua on 11/3/19.
+//  Copyright © 2019 Cristian Paniagua. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
